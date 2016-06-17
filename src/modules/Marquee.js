31a7a582fa75577a77dec7eae49d1f91d7e0ff9d
Allison Day <day.allison@gmail.com>
2016-06-17 00:59:18 -0700

    Allow parallax to zoom out instead of in; change zoom percentage to 25%
    Signed-off-by: Allison Day <day.allison@gmail.com>

--- src/modules/Marquee.js
+++ src/modules/Marquee.js
@@ -35,6 +35,11 @@ export default class Marquee extends Component {
     window.addEventListener('resize', this._handleScroll);
     window.addEventListener('resize', this._setBackgroundColorIndex);
     this._setBackgroundColorIndex();
+    this._getBackgroundImageRatio();
+
+    setTimeout(() => {
+      this._handleScroll();
+    }, 100);
   }
 
   componentWillUnmount () {
@@ -68,14 +73,16 @@ export default class Marquee extends Component {
   }
 
   _handleScroll () {
+    const { size, zoom, zoomPercentage } = this.props;
+
     let marqueeOriginalHeight = window.innerHeight * 0.75;
     if (window.innerWidth < PALM_BREAKPOINT) {
-      if (this.props.size === 'small') {
+      if (size === 'small') {
         marqueeOriginalHeight = 270;
       } else {
         marqueeOriginalHeight = 300;
       }
-    } else if (this.props.size === 'small') {
+    } else if (size === 'small') {
       marqueeOriginalHeight = window.innerHeight * 0.60;
     }
 
@@ -88,6 +95,12 @@ export default class Marquee extends Component {
     let marqueeRatio = marquee.offsetWidth / marqueeOriginalHeight;
     let backgroundHeight = 0;
     let backgroundWidth = 0;
+
+    let startPositionPercentage = 1;
+    if (zoom === 'out') {
+      startPositionPercentage = 1 + (zoomPercentage / 100);
+    }
+
     if (backgroundRatio > marqueeRatio) {
       // constrained by marquee height
       backgroundHeight = marqueeOriginalHeight;
@@ -106,7 +119,7 @@ export default class Marquee extends Component {
         marqueeText.style.height = 0;
         marqueeText.style.top = `${marqueeOriginalHeight}px`;
       } else if (marqueeTop > 0) {
-        marqueeText.style.height = marqueeOriginalHeight;
+        marqueeText.style.height = `${marqueeOriginalHeight}px`;
         marqueeText.style.top = 0;
       } else {
         marqueeText.style.height = `${marqueeOriginalHeight + marqueeTop}px`;
@@ -119,17 +132,27 @@ export default class Marquee extends Component {
     }
 
     let positionPercentage;
-    if (marqueeTop < 0 && marqueeTop >= -marqueeOriginalHeight) {
-      positionPercentage = ((positionRatio) * 25 + 100) / 100;
-      console.log(positionPercentage);
+    let zoomPositionRatio = positionRatio;
+    let finalPositionPercentage = 1;
+    if (zoom === 'out') {
+      finalPositionPercentage = 1 + (zoomPercentage / 100);
     } else {
-      positionPercentage = 1.25;
+      zoomPositionRatio = 1 - positionRatio;
     }
+
+    if (marqueeTop < 0 && marqueeTop >= -marqueeOriginalHeight) {
+      positionPercentage = (zoomPositionRatio * zoomPercentage + 100) / 100;
+    } else if (marqueeTop >= 0) {
+      positionPercentage = startPositionPercentage;
+    } else {
+      positionPercentage = finalPositionPercentage;
+    }
+
     marquee.style.backgroundSize = `${backgroundWidth * positionPercentage}px ${backgroundHeight * positionPercentage}px`;
   }
 
   _setBackgroundColorIndex () {
-    let { darkTheme } = this.props;
+    const { darkTheme } = this.props;
 
     if (window.innerWidth < PALM_BREAKPOINT) {
       this.setState({ colorIndex: LIGHT_COLORINDEX });
@@ -139,7 +162,7 @@ export default class Marquee extends Component {
   }
 
   render () {
-    let { backgroundImage, flush, headlineSize, headline, justify, link, linkIcon, linkText, onClick, subHeadline } = this.props;
+    const { backgroundImage, flush, headlineSize, headline, justify, link, linkIcon, linkText, onClick, subHeadline } = this.props;
 
     let classes = classnames(
       CLASS_ROOT,
@@ -209,7 +232,9 @@ Marquee.propTypes = {
   responsiveBackgroundPosition: PropTypes.oneOf(['left', 'center', 'right']),
   separator: PropTypes.bool,
   size: PropTypes.oneOf(['small', 'large']),
-  subHeadline: PropTypes.string
+  subHeadline: PropTypes.string,
+  zoom: PropTypes.oneOf(['in', 'out', 'none']),
+  zoomPercentage: PropTypes.number
 };
 
 Marquee.defaultProps = {
@@ -220,5 +245,7 @@ Marquee.defaultProps = {
   linkText: 'Learn More',
   responsiveBackgroundPosition: 'center',
   separator: false,
-  size: 'large'
+  size: 'large',
+  zoom: 'out',
+  zoomPercentage: 25
 };
