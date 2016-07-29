4203270e3c3009ca18d15873cc5c6fab21d3c0bf
Emmanuel Pilande <manny.pilande@hpe.com>
2016-07-28 19:11:03 -1000

    Remove accordion docs page
    Accordion docs are in grommet-docs

--- src/develop/Develop.js
+++ src/develop/Develop.js
@@ -19,7 +19,6 @@ var DashboardDoc = require('./patterns/DashboardDoc');
 var NavigationDoc = require('./patterns/NavigationDoc');
 var MarqueeDoc = require('./components/MarqueeDoc');
 var WorldMapDoc = require('./components/WorldMapDoc');
-var AccordionDoc = require('./components/AccordionDoc');
 var CalloutDoc = require('./components/CalloutDoc');
 
 //hjjs configuration
@@ -49,7 +48,6 @@ var CONTENTS = [
     contents: [
       {route: 'develop_box', label: 'Box', component: BoxDoc},
       {route: 'develop_marquee', label: 'Marquee', component: MarqueeDoc},
-      {route: 'develop_accordion', label: 'Accordion', component: AccordionDoc},
       {route: 'develop_callout', label: 'Callout', component: CalloutDoc},
       {route: 'develop_world-map', label: 'WorldMap', component: WorldMapDoc}
     ]
